9176f9dec5bbfb0d48fe606ac89b8cb96cc7c7ac
Michal Saraz <saraz.m@seznam.cz>
2025-03-26 16:34:27 +0100

    Refactor database initialization and configuration; move environment and configuraton files to WebApi folder

--- EshopApi.WebApi/Program.cs
+++ EshopApi.WebApi/Program.cs
@@ -1,16 +1,21 @@
+using DotNetEnv.Configuration;
 using EshopApi.Infrastructure;
+using EshopApi.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .AddEnvironmentVariables();
+    .AddEnvironmentVariables()
+    .AddDotNetEnv();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<EshopDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MasterConnection")));
+
+builder.Services.AddScoped<InitialData>();
 
 var app = builder.Build();
 
@@ -21,5 +26,14 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var initialData = services.GetRequiredService<InitialData>();
+    var context = services.GetRequiredService<EshopDbContext>();
+    
+    await initialData.InitializeDatabaseAsync(context);
+}
 
 app.Run();
